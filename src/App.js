42239a725bba7fdf2e6ee1b2d181c53072991d86
fatfan <fatfan1020@gmail.com>
2019-11-17 12:14:21 +0800

    使用 contextType

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Theme = React.createContext('yellow')
+function Button(props) {
+  return(
+
+      <button onClick={props.theme.setTheme}
+              className={props.theme.color}>
+        {props.theme.color}
+      </button>
+  )
+
+}
+class Box extends React.Component{
+  static contextType = Theme;
+  render() {
+    return(
+      <div >
+        <Button theme={this.context} />
+        <div>{this.props.children}</div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      theme: {
+        color: "red",
+        setTheme: (color)=> {
+          console.log("被调用了")
+          console.log(this.state)
+          if(this.state.theme.color === "red"){
+            this.setState({
+                theme: {
+                  ...this.state.theme,
+                  color: "yellow"
+                }
+              }
+            )
+          }else {
+            this.setState({
+                theme: {
+                  ...this.state.theme,
+                  color: "red"
+                }
+              }
+            )
+          }
+
+
+        }
+      }
+    }
+  }
+  render() {
+    return (
+        <Theme.Provider  value={this.state.theme}>
+          <Box>
+            <button>我是props.children</button>
+          </Box>
+        </Theme.Provider >
+    )
+  }
+}
+
+
+export  default App
+
+
+
